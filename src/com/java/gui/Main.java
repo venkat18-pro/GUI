11da0c9b2c778c61faf79b4ee5fad7bfa5f1d414
Venkatesh <viv7012@gmail.com>
2022-02-18 18:17:36 +0530

    JFrame class

---
+++ src/com/java/gui/Main.java
@@ -0,0 +1,19 @@
+package com.java.gui;
+
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		JFrame frame = new JFrame();
+		
+		frame.setTitle("venkat");
+		
+		frame.setSize(500, 500);
+		
+		frame.setVisible(true);
+
+	}
+
+}
